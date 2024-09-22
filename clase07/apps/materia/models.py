c1537eb4690344130759bd920c8c0add240df8d0
ozunacristian <ozunacristianj@gmail.com>
2024-09-22 05:16:06 -0300

    migraciones correctas.

--- clase07/apps/materia/models.py
+++ clase07/apps/materia/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Materia(models.Model):
   nombre = models.CharField(max_length=50)
+  carrera = models.ForeignKey("carrera.Carrera", on_delete=models.CASCADE)
 
   def __str__(self):
     return {self.nombre}
--- clase07/apps/usuario/models.py
+++ clase07/apps/usuario/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Usuario(AbstractUser):
-  telefono = models.CharField(max_length=15)
+class Usuario(models.Model):
+  nombre = models.CharField(max_length=15)
+
+  def __str__(self):
+    return self.telefono
 
 
