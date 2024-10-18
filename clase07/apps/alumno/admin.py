4fcdd3c2893685169d3f625acb35987b521b1413
ozunacristian <ozunacristianj@gmail.com>
2024-10-18 16:57:33 -0300

    gitignore adecuado. Se registra alumno a panel de admin.

--- clase07/apps/alumno/admin.py
+++ clase07/apps/alumno/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.alumno.models import Alumno
 
 # Register your models here.
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+  list_display = [nombre, apellido, dni, fecha_nacimiento, direccion, telefono, email, activo,]
+  search_fields = [nombre, apellido, fecha_nacimiento, email, activo,]
+  list_filter = []
